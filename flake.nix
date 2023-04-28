7d9f9a789e51ff22d85e48009bf75593645d8de1
Matthijs Steen <emailmatthijs@gmail.com>
2023-04-28 11:34:39 +0200

    refactor: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,37 @@
 {
   description = "NixOS VSCode server";
 
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs = {
     self,
     nixpkgs,
-  }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-    auto-fix-vscode-server = pkgs.callPackage ./pkgs/auto-fix-vscode-server.nix { };
-  in {
-    nixosModule = import ./modules/vscode-server;
-    nixosModules.default = self.nixosModule;
-    nixosModules.home = import ./modules/vscode-server/home.nix;
-    checks.x86_64-linux.auto-fix-vscode-server = auto-fix-vscode-server;
-  };
+    flake-utils,
+  }:
+    {
+      nixosModule = self.nixosModules.default; # Deprecrated, but perhaps still in use.
+      nixosModules.default = import ./modules/vscode-server;
+      nixosModules.home = self.homeModules.default; # Backwards compatiblity.
+      homeModules.default = import ./modules/vscode-server/home.nix; # Consistent with homeConfigurations.
+    }
+    // (let
+      inherit (flake-utils.lib) defaultSystems eachSystem;
+    in
+      eachSystem defaultSystems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs.lib) hasSuffix optionalAttrs;
+        auto-fix-vscode-server = pkgs.callPackage ./pkgs/auto-fix-vscode-server.nix { };
+      in
+        # The package depends on `inotify-tools` which is only available on Linux.
+        optionalAttrs (hasSuffix "-linux" system) {
+          packages = {
+            inherit auto-fix-vscode-server;
+            default = auto-fix-vscode-server;
+          };
+          checks = {
+            inherit auto-fix-vscode-server;
+          };
+        }));
 }
